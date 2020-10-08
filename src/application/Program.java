ff4f08fc7fa621c9bedbe7f3495e122e0a7d7068
Mauro Melo <mauromelo1@gmail.com>
2020-10-08 19:00:59 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 
-		while (! chessMatch.getCheckMate()) {
+		while (!chessMatch.getCheckMate()) {
 
 			try {
 				UI.clearScreen();
@@ -36,17 +36,20 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
-				if(chessMatch.getPromoted() != null) {
+
+				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();						
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
-				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				Utils.pausar(4);
@@ -54,7 +57,7 @@ public class Program {
 				System.out.println(e.getMessage());
 				Utils.pausar(4);
 			}
-			
+
 			UI.clearScreen();
 			UI.printMatch(chessMatch, captured);
 
